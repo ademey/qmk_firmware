a126e8369f25b949f3ff985e02fddb821df2f05a
Anne Demey <anneyvdemey@gmail.com>
2019-09-29 15:19:52 -0500

    Adding game layer for dz60

--- keyboards/dz60/keymaps/spacewoman/keymap.c
+++ keyboards/dz60/keymaps/spacewoman/keymap.c
@@ -21,8 +21,9 @@
 
 #define _BL 0
 #define _WIN 1
-#define _FN 2
-#define _CL 3
+#define _GAME 2
+#define _FN 3
+#define _CL 4
 
 #define FN_SPC LT(_FN, KC_SPC)
 
@@ -64,6 +65,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LSFT, XXXXXXX, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,          XXXXXXX,
         KC_LCTL,          KC_LGUI, KC_LALT, XXXXXXX,          FN_SPC ,          XXXXXXX, KC_RALT, KC_RGUI, XXXXXXX, MO(_CL),          KC_RCTL),
 
+    [_GAME] = LAYOUT(
+        KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , XXXXXXX, KC_BSPC,
+        KC_TAB ,          KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_CAPS, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,          KC_ENT ,
+        KC_LSFT, XXXXXXX, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,          XXXXXXX,
+        KC_LCTL,          KC_LGUI, KC_LALT, XXXXXXX,          KC_SPC ,          XXXXXXX, KC_RALT, KC_RGUI, XXXXXXX, MO(_CL),          KC_RCTL),
+
 
     /* Keymap : Second Layer (WASD and Media)
    * ,-----------------------------------------------------------.
@@ -103,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_CL] = LAYOUT(
         KC_GRV , MACRO_1, MACRO_2, MACRO_3, MACRO_4, MACRO_5, _______, _______, _______, _______, _______, _______, KC_PSCR, XXXXXXX, KC_SLEP,
         _______,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, RESET  ,
-        _______,          RGB_STA, RGB_BRE, RGB_RAI, RGB_SWI, RGB_SNA, RGB_KNI, RGB_GRA, _______, _______, _______, _______,          _______,
+        _______,          RGB_STA, RGB_BRE, RGB_RAI, RGB_SWI, RGB_SNA, RGB_KNI, RGB_GRA, _______, _______, _______, _______,          TO(_GAME),
         _______, _______, BL_DEC , BL_TOGG, BL_INC , BL_STEP, _______, _______, _______, _______, _______, _______, TO(_WIN),         _______,
         _______,          _______, _______, _______,          _______,          _______, _______, _______, _______, _______,          TO(_BL))
 
