a8934d54b5f620b5c5d5ec63ade6dafdebaa8aa8
Anne Demey <anneyvdemey@gmail.com>
2020-02-12 08:06:44 -0600

    Swapped tab for escape

--- keyboards/walletburner/neuron/keymaps/spacewoman/keymap.c
+++ keyboards/walletburner/neuron/keymaps/spacewoman/keymap.c
@@ -1,3 +1,4 @@
+// make walletburner/neuron:spacewoman
 #include QMK_KEYBOARD_H
 
 #if (__has_include("secrets.h"))
@@ -38,10 +39,10 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_QWERTY] = LAYOUT(
-		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P, KC_BSPC, 
-		KC_CESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,         KC_ENT, 
-		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,       SFTSLSH, 
-		         KC_LGUI, KC_RALT,          LOWERSP, LOWERSP, RAISESP,          NAV,     FN), 
+		KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, 
+		KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,             KC_ENT, 
+		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,           SFTSLSH, 
+		         KC_LGUI, KC_RALT,          LOWERSP, LOWERSP, RAISESP,          NAV,     FN),
 
 	[_RAISE] = LAYOUT(
 		KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL , 
@@ -50,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		         _______, _______,          _______, _______, _______,          _______, _______),
 
 	[_LOWER] = LAYOUT(
-		KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, 
+		KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, 
 		_______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR,          KC_RCBR, 
 		_______, _______, _______, _______, _______, _______, _______, _______, KC_BSLS, KC_PIPE,          _______, 
 		         _______, _______,          _______, _______, _______,          _______, _______),
@@ -64,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_FN] = LAYOUT(
 		KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
 		KC_CAPS, MACRO_1, MACRO_2, MACRO_3, MACRO_4, MACRO_5, _______, _______, _______, _______,          _______, 
-		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, 
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          CG_TOGG, 
 		         RESET,   _______,          _______, _______, _______,          _______, _______)
 };
 
